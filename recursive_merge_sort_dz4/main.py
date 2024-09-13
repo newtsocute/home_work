56de2a45e3924e0d32005e6a392c40e7dc4e69b6
newtsocute <lapinslava15@gmail.com>
2024-09-13 17:13:43 +1000

    переделал немного 13.09
    добавил бэнчмарк

--- recursive_merge_sort_dz4/main.py
+++ recursive_merge_sort_dz4/main.py
@@ -1,45 +1,43 @@
-def mergesort(arr):
-    if len(arr) > 1:
-        mid = len(arr) // 2
-        left = arr[:mid]
-        right = arr[mid:]
-
-        mergesort(left)
-        mergesort(right)
-
-        i = j = k = 0
-
-        while i < len(left) and j < len(right):
-            if left[i] < right[j]:
-                arr[k] = left[i]
-                i += 1
-            else:
-                arr[k] = right[j]
-                j += 1
-            k += 1
-
-        while i < len(left):
+import time
+import random
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+    merge_sort(left)
+    merge_sort(right)
+    
+    i = j = k = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
             arr[k] = left[i]
             i += 1
-            k += 1
-
-        while j < len(right):
+        else:
             arr[k] = right[j]
             j += 1
-            k += 1
+        k += 1
+    while i < len(left):
+        arr[k] = left[i]
+        i += 1
+        k += 1
+    while j < len(right):
+        arr[k] = right[j]
+        j += 1
+        k += 1
+    return arr
+
+def test_merge_sort():
+    arr = [random.randint(0, 100) for _ in range(10000)]
+    merge_sort(arr)
+    print(arr)
 
 
-def test_func():
-    data = [
-        [31, 21, 91, 33, 8, 82, 10],
-        [5, 7, 6, 6, 1, 3],
-        [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-    ]
+time_start = time.perf_counter()
+test_merge_sort()
+time_end = time.perf_counter()
+round_time = time_end - time_start
+print(f"Time taken: {round_time:.5f} seconds")
 
-    for i, data in enumerate(data):
-        print(f"Тест {i+1}: Входные данные: {data}")
-        mergesort(data)
-        print(f"Тест {i+1}: Выходны данные: {data}\n")
-
-
-test_func()
+    
